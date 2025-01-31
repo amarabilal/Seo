dafc3e12166b79dbe6628f4666df2a70dfcb36a6
AMARA Bilal <amara.bilalpro@gmail.com>
2025-01-31 17:04:29 +0100

    Update twitter-card and Meta Tags

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.zigou-plex.fr',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://www.zigou-plex.fr', // Remplacez par votre URL
+  generateRobotsTxt: true, // Générer un fichier robots.txt
+  exclude: ['/404'], // Exclure certaines pages
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      'https://www.zigou-plex.fr/sitemap.xml',
+    ],
+  },
+}
